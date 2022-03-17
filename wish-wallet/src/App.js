9165f9953f1e8e69d0bdf3868907a96aef7936fb
Martin <escorpmartin97@gmail.com>
2022-03-17 18:41:07 -0300

    feat: utiliza rauter e coloca pagina principal na rota wallet

--- wish-wallet/src/App.js
+++ wish-wallet/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import kleverLogo from './images/logo.svg';
 import { Header } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <Header logo={ kleverLogo }/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/wallet" element={ <Header logo={ kleverLogo }/> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
