62488bb1958e3e6799a96fffdffb13bcab9cd194
Martin <escorpmartin97@gmail.com>
2022-03-17 18:46:49 -0300

    feat: quando rota padrao for chamada ela vai renderizar o /wallet

--- wish-wallet/src/App.js
+++ wish-wallet/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import kleverLogo from './images/logo.svg';
 import { Header } from './components';
 
@@ -8,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={ <Navigate to="/wallet" /> } />
         <Route path="/wallet" element={ <Header logo={ kleverLogo }/> } />
       </Routes>
     </BrowserRouter>
