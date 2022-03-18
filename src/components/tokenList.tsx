c838bcc6c14a7764d99221c7fae82249026302d6
tiagolpires <tiagolpires@hotmail.com>
2022-03-17 22:05:39 -0300

    feat: add empty tokenList message

--- src/components/tokenList.tsx
+++ src/components/tokenList.tsx
@@ -8,16 +8,24 @@ interface TokenListProps {
 const TokenList: React.FC<TokenListProps> = ({ tokens }) => {
   return (
     <div className="w-full max-w-xl">
-      <div className="text-md flex w-full items-center justify-between font-bold">
-        <h2>Tokens</h2>
-        <h2>Balance</h2>
-      </div>
-      <div>
-        {tokens &&
-          tokens.map((token: TokenInterface, index: number) => (
-            <Token key={index} token={token.token} balance={token.balance} />
-          ))}
-      </div>
+      {tokens.length > 0 && (
+        <>
+          <div className="text-md flex w-full items-center justify-between font-bold">
+            <h2>Tokens</h2>
+            <h2>Balance</h2>
+          </div>
+          <div>
+            {tokens.map((token: TokenInterface, index: number) => (
+              <Token key={index} token={token.token} balance={token.balance} />
+            ))}
+          </div>
+        </>
+      )}
+      {tokens.length === 0 && (
+        <h2 className="text-center text-xl ">
+          You have not created any tokens yet 🛸
+        </h2>
+      )}
     </div>
   )
 }
--- src/helpers/localStorage.tsx
+++ src/helpers/localStorage.tsx
@@ -3,7 +3,7 @@ import { TokenInterface } from '../components/token'
 const LocalStorage = {
   load(key: string): TokenInterface[] | any {
     const stored = localStorage.getItem(key)
-    return stored == null ? undefined : JSON.parse(stored)
+    return stored == null ? [] : JSON.parse(stored)
   },
   store(key: string, value: string) {
     localStorage.setItem(key, JSON.stringify(value))
