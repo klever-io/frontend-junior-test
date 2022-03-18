7d16fc1714ed0ad85c818511e36f5c5ae9ea58ce
Martin <escorpmartin97@gmail.com>
2022-03-18 15:52:40 -0300

    feat: adiciona butao nos items e adiciona th com non-breaking space

--- wish-wallet/src/components/TableWallet/index.js
+++ wish-wallet/src/components/TableWallet/index.js
@@ -2,11 +2,41 @@ import React from 'react';
 import { Container } from './styles';
 
 function TableWallet() {
+
+  const walletMock = [
+    { token: "KLV", value: "23,233.43" },
+    { token: "DVK", value: "28,260.42" },
+    { token: "KFI", value: "10" }
+  ]
+
   return (
     <Container>
-      <p>hello</p>
+      <table>
+        <thead>
+          <tr>
+            <th>&nbsp;</th>
+            <th>Tokens</th>
+            <th>Balance</th>
+          </tr>
+        </thead>
+          {
+            walletMock.map(({ token, value }, i) => (
+              <tbody key={ i }>
+                <tr>
+                  <td>
+                    <button>edit</button>
+                  </td>
+                  <td>{ token }</td>
+                  <td>{ value }</td>
+                </tr>
+              </tbody>
+            ))
+          }
+      </table>
     </Container>
   );
 }
 
 export default TableWallet;
+
+// reference: https://www.homehost.com.br/blog/criar-sites/tabela-html/
