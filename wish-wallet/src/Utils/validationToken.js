696719b6c721064cd79407567b228faf762df314
Martin <escorpmartin97@gmail.com>
2022-03-21 08:09:16 -0300

    feat: funcoes enviam messsage de erro

--- wish-wallet/src/Utils/validationToken.js
+++ wish-wallet/src/Utils/validationToken.js
@@ -5,17 +5,15 @@ const tokenExist = (token) => {
   const listTokens = getToken('wallet');
   if (!listTokens) return true;
   const findToken = listTokens.some((item) => item.token === token);
-  return !findToken;
+  return findToken ? 'Token existe!' : true;
 };
 
 const inputValidate = (token, balance) => {
   const walletLength = token.length >= 3 && balance.length >= 1;
-  return walletLength;
+  return walletLength ? walletLength : 'Compos obrigatorios';
 };
 
 export const validations = (token, balance) => {
-  if (tokenExist(token) && inputValidate(token, balance)) {
-    return true;
-  }
-  return false;
+  tokenExist(token)
+  inputValidate(token, balance)
 };
