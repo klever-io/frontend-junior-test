3ba4b63b0dd68da6bfca9b3eab9b908631940607
Martin <escorpmartin97@gmail.com>
2022-03-21 00:01:02 -0300

    feat: adiciona referencia de codigo

--- wish-wallet/src/Utils/confirmRemove.js
+++ wish-wallet/src/Utils/confirmRemove.js
@@ -2,3 +2,5 @@ export const confirmRemove = (remove) => {
   const confirmRemoveItem = confirm("desea remover o item?");
   return confirmRemoveItem ? remove() : !false;
 };
+
+// reference: https://www.devmedia.com.br/alert-em-javascript/37208
