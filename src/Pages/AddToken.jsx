c3c6dabe73bc843bc00f04e351c8c7eac61da261
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2023-02-15 12:31:21 -0300

    feat: html done

--- src/Pages/AddToken.jsx
+++ src/Pages/AddToken.jsx
@@ -3,7 +3,40 @@ import Header from '../Components/Header'
 
 function AddToken() {
   return (
-    <div><Header /></div>
+    <div>
+      <Header />
+      <div>
+        <span>Add Token</span>
+        <button
+          type="button"
+        >
+          Back
+        </button>
+        <form>
+          <label htmlFor="token">
+            Token
+            <input
+              type="text"
+              name="token"
+              id="token" 
+            />
+          </label>
+          <label htmlFor="balance">
+            Balance
+            <input
+              type="number"
+              name="balance"
+              id="balance" 
+            />
+          </label>
+        </form>
+        <button
+          type="button"
+        >
+          Save
+        </button>
+      </div>
+    </div>
   )
 }
 
