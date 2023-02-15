73d98dd89c5b16f996a235a0dda36c21dccb6d7a
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2023-02-14 21:01:56 -0300

    style: add css in header, klever logo, shooting star and tag span

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,19 +2,19 @@ import { createBrowserHistory } from '@remix-run/router';
 import React from 'react'
 import KleverLogo from '../assets/logo.svg'
 import Star from '../assets/shooting-star.svg'
+import '../CSS/header.css'
 
 function Header() {
   const history = createBrowserHistory();
   const locate = history.location.pathname;
   return (
     <header>
-        <div>
-          <img src={ KleverLogo } alt="logo Klever" />
-          <span>Klever</span>
+        <div className="klever-logo-container">
+          <img id="klever-logo" src={ KleverLogo } alt="logo Klever" />
         </div>
-        <div>
-          <img src={ Star } alt="shooting star" />
-          <span>Wish Wallet</span>
+        <div className="star-container">
+          <img id="shooting-star" src={ Star } alt="shooting star" />
+          <span id="wish-span">Wish Wallet</span>
           { locate === "/" && 
               <button 
               type="button"
