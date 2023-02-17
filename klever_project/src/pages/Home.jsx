be8eeb8bbf05d0adda0bdc4a6f2953f4251e2235
Larissa <larisssa_rga@hotmail.com>
2023-02-17 14:29:34 -0300

    [add] - table and button at Page Home

--- klever_project/src/pages/Home.jsx
+++ klever_project/src/pages/Home.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
+
   return (
-    <div>Home</div>
+    <div>
+      <div>
+        <h1> Wish Wallet</h1>
+        <Link to="/AddToken">
+        <button
+        type='button'
+        >
+          Add Token 
+        </button>
+        </Link>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th> Token </th>
+            <th> Balance </th>
+          </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+            <td>token</td>
+            <td>balance</td>
+            <td>
+              <Link to={ `/EditToken` }>
+              edit
+              </Link>
+            </td>
+          </tr>
+        </tbody>
+
+      </table>
+    </div>
   )
 }
