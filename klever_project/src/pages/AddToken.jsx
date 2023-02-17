44e9f2a18dcf0ed9c0295790b8306262532bee53
Larissa <larisssa_rga@hotmail.com>
2023-02-17 14:35:18 -0300

    [add] - form and button back at page AddToken

--- klever_project/src/pages/AddToken.jsx
+++ klever_project/src/pages/AddToken.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function AddToken() {
   return (
-    <div>AddToken</div>
+    <div>
+      <h1> Wish Wallet</h1>
+      <div>
+        <h2>Add Token</h2>
+        <Link to="">
+        <button
+        type='button'
+        >
+        Voltar 
+        </button>
+        </Link>
+      </div>
+    <div>
+      Token
+      <input type="text" />
+    </div>
+
+    <div>
+    Balance
+      <input type="number" />
+    </div>
+    <div>
+      <button type="button" >
+        Salvar
+      </button>
+    </div>
+  </div>
   )
 }
