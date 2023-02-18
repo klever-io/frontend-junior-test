d1d0e1f806628cbd220e518f0d3ac8a8944dcf5f
luc0liv <luoliveiradacunha@gmail.com>
2023-02-18 00:32:02 -0300

    feat: create Form component and it's props with basic styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React from "react";
+import Button from "./Button";
 
-function Form() {
+function Form({ value, onInputChange, onButtonClick, isEdit }) {
   return (
-    <div>Form</div>
+    <form className="wallet-form">
+      <label htmlFor="token">
+        Token
+        <input
+          type="text"
+          id="token"
+          name="token"
+          value={ value.token }
+          onChange={ onInputChange }
+        />
+      </label>
+      <label htmlFor="balance">
+        Balance
+        <input
+          type="text"
+          id="balance"
+          name="balance"
+          value={ value.balance }
+          onChange={ onInputChange }
+        />
+      </label>
+    <div className={ isEdit ? 'form-buttons-edit' : 'form-buttons'}>
+    { isEdit && (
+        <Button name="Remove" kind="remove" onButtonClick={ onButtonClick.remove } />
+      )}
+      <Button name="Save" onButtonClick={ onButtonClick.save } />
+    </div>
+    </form>
   );
 }
 
