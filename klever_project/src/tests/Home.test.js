df86d2ac4dd3d10cd7a251d9da84137cae7113ef
Larissa <larisssa_rga@hotmail.com>
2023-02-17 23:33:24 -0300

    [add] - change on Home.test.js about route

--- klever_project/src/tests/Home.test.js
+++ klever_project/src/tests/Home.test.js
@@ -1,23 +1,29 @@
 import App from "../App";
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from "../helper/renderWithRouter";
 
 describe('Teste referente a página de Home', () => {
-  renderWithRouter(
+  const { history } = renderWithRouter(
     <App />
   );
-  it('Renderiza uma tabela com token e balance', () => {
+  it('Renderiza uma tabela com token e balance e clica no botão add', () => {
     const title = screen.getByRole('heading', { name: /wish wallet/i })
 
-    const addButton = screen.getByRole('button', { name: /add token/i })
-
     expect(title).toBeInTheDocument()
-    expect(addButton).toBeInTheDocument()
 
     expect(screen.getByRole('columnheader', { name: /token/i })).toBeInTheDocument();
     expect(screen.getByRole('columnheader', { name: /balance/i })).toBeInTheDocument();
 
     const rows = screen.getAllByRole('row');
     expect(rows.length).toBe(1);
+
+    const addButton = screen.getByRole('button', { name: /add token/i })
+    expect(addButton).toBeInTheDocument()
+    
+    userEvent.click(addButton);
+
+    const path = history.location.pathname;
+    expect(path).toBe('/AddToken');
   });
 });
