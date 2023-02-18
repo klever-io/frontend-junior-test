91e03ea04ba1dcc0221da9a0e30bd89d4722d259
luc0liv <luoliveiradacunha@gmail.com>
2023-02-18 00:32:51 -0300

    feat: control Form through AddOrEditToken page

--- src/pages/AddOrEditToken.jsx
+++ src/pages/AddOrEditToken.jsx
@@ -1,11 +1,24 @@
 import WalletHeader from '../components/WalletHeader';
 import Form from '../components/Form';
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 function AddOrEditToken() {
+  const location = useLocation();
+  const [inputValues, setInputValues] = useState({ token: '', balance: '' });
+
+  const handleChange = ({ target }) => {
+    setInputValues({ ...inputValues, [target.name]:target.value });
+  }
+
   return (
     <>
     <WalletHeader />
-    <Form />
+    <Form 
+      isEdit={ location.pathname === '/edit-token' }
+      value={ inputValues }
+      onInputChange={ (e) => handleChange(e) }
+    />
     </>
   )
 }
