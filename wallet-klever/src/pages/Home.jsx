7f854ae45a42862ff5cfcc22e204abf8c850bb1c
LanaTFernandes <lanatf@yahoo.fr>
2023-02-21 14:50:26 -0300

    add AddToken button on page Home

--- wallet-klever/src/pages/Home.jsx
+++ wallet-klever/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Header from '../components/Header';
+import { useNavigate } from 'react-router-dom';
+
 
  function Home() {
+
+  const navigate = useNavigate();
+
+  const AddTokenClick = () => {
+    navigate('/addToken');
+  }
+
   return (
     <div>
       <Header />
+      <button onClick={ AddTokenClick }>Add Token</button>
     </div>
   )
 }
