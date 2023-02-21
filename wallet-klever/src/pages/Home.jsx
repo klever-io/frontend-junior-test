6c7c83b59098681e043d7cbdcb6d7b571e6218e7
LanaTFernandes <lanatf@yahoo.fr>
2023-02-21 15:49:44 -0300

    add table and edit button on home page

--- wallet-klever/src/pages/Home.jsx
+++ wallet-klever/src/pages/Home.jsx
@@ -11,10 +11,34 @@ import { useNavigate } from 'react-router-dom';
     navigate('/addToken');
   }
 
+  const EditTokenClick = () => {
+    navigate(`/editToken`);
+  }
+
   return (
     <div>
       <Header />
       <button onClick={ AddTokenClick }>Add Token</button>
+        <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Tokens</th>
+                    <th>Balance</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <tr>KLV</tr>
+                        <tr>10,250.50</tr>
+                        <button onClick={() => EditTokenClick()}>Edit Token</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
     </div>
   )
 }
