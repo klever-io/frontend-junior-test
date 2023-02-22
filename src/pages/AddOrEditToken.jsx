f619536dc4bd8eef0caa5db13d077e3f2b20f49a
luc0liv <luoliveiradacunha@gmail.com>
2023-02-21 21:20:36 -0300

    feat: load edit page with fields filled

--- src/pages/AddOrEditToken.jsx
+++ src/pages/AddOrEditToken.jsx
@@ -1,29 +1,47 @@
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import WalletHeader from '../components/WalletHeader';
 import Form from '../components/Form';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 
 function AddOrEditToken() {
   const location = useLocation();
   const navigate = useNavigate();
+  const { id } = useParams();
   const [inputValues, setInputValues] = useState({ token: '', balance: '' });
   const [errorMessage, setErrorMessage] = useState({ token: '', balance: '' });
-  
+
+  useEffect(() => {
+    if (location.pathname.includes('/edit-token')) {
+      const tokens = JSON.parse(localStorage.getItem('tokenList'));
+      if (tokens) {
+        const editValues = tokens.find((token) => token.token === id);
+        setInputValues({
+          token: editValues.token,
+          balance: editValues.balance,
+        });
+      }
+    }
+  }, [location, id]);
+
   const handleChange = ({ target }) => {
-    setInputValues({ ...inputValues, [target.name]:target.value });
-  }
-  
-  const isButtonDisabled = () => inputValues.token === '' || inputValues.balance === '';
+    setInputValues({ ...inputValues, [target.name]: target.value });
+  };
+
+  const isButtonDisabled = () =>
+    inputValues.token === '' || inputValues.balance === '';
 
   const saveToken = () => {
-    if (!JSON.parse(localStorage.getItem('tokenList'))) {
+    const tokens = JSON.parse(localStorage.getItem('tokenList'));
+    if (!tokens) {
       localStorage.setItem('tokenList', JSON.stringify([inputValues]));
     } else {
       const storageTokenList = JSON.parse(localStorage.getItem('tokenList'));
-      const validateIfTokenExists = storageTokenList.some((item) => item.token === inputValues.token);
+      const validateIfTokenExists = storageTokenList.some(
+        (item) => item.token === inputValues.token
+      );
       if (validateIfTokenExists) {
         setErrorMessage({ token: 'That token already exists!' });
-        return
+        return;
       }
       setErrorMessage('');
       const tokenList = [...storageTokenList, inputValues];
@@ -37,31 +55,31 @@ function AddOrEditToken() {
 
   const saveOrEdit = () => {
     if (location.pathname === '/add-token') {
-      return saveToken()
+      return saveToken();
     }
     if (location.pathname === '/edit-token') {
-      return editToken()
+      return editToken();
     }
-  }
+  };
 
   const tokenControl = {
     save: saveOrEdit,
     remove: removeToken,
-  }
+  };
 
   return (
     <>
-    <WalletHeader />
-    <Form 
-      isEdit={ location.pathname === '/edit-token' }
-      value={ inputValues }
-      onInputChange={ (e) => handleChange(e) }
-      onButtonClick={ tokenControl }
-      errorMessage={ errorMessage }
-      isButtonDisabled={ isButtonDisabled() }
-    />
+      <WalletHeader />
+      <Form
+        isEdit={ location.pathname === '/edit-token' }
+        value={ inputValues }
+        onInputChange={ (e) => handleChange(e) }
+        onButtonClick={ tokenControl }
+        errorMessage={ errorMessage }
+        isButtonDisabled={ isButtonDisabled() }
+      />
     </>
-  )
+  );
 }
 
 export default AddOrEditToken;
