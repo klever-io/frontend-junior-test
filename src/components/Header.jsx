b7beb075e7dcc80c4d2e02bdfb340c76c7749b09
Bernardo Marques <bernardomp.dev@gmail.com>
2023-02-23 18:58:53 -0300

    initial commit, feat: Header created

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../assets/logo.svg';
+
+function Header() {
+  return (
+    <div>
+      <img src={ Logo } alt="oi" width="300px"/>
+    </div>
+  )
+}
+
+export default Header
