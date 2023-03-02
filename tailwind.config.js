ccdd14364ac217ae060f1c0b57d2f4088eea6e18
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-02 19:08:17 -0300

    style: Creates custom colors in tailwind using Klever's color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        klever: {
+          'enabled-button': '#AA33B5',
+          'disabled-button': '#501c56',
+          'remove-button': '#920000',
+          'back-button': '#646464',
+          'background': '#13152A',
+        },
+      }
+    },
   },
   plugins: [],
 }
