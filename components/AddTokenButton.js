cf2759e68a55725ac86a702bf9240c6de7d20519
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-02 20:20:35 -0300

    feat: Creates "Add Token" button

---
+++ components/AddTokenButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddTokenButton() {
+  return (
+    <div className='p-2 rounded-md font-semibold text-white bg-klever-enabled-button'>
+      Add Token
+    </div>
+  );
+}
