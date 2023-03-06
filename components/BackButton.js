42e9288e97617f0cfa1d70399bc0df370633731c
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-06 16:55:15 -0300

    feat: Back button now displays current page

--- components/BackButton.js
+++ components/BackButton.js
@@ -8,15 +8,26 @@ export default function BackButton() {
   const router = useRouter();
 
   const goToPreviousPage = () => {
-    router.back()
+    router.push('/')
   }
 
   return (
-    <button
-    onClick={goToPreviousPage}
-    className='p-2 sm:p-3 sm:px-5 rounded-md font-semibold
-    text-white bg-klever-back-button'>
-      Voltar
-    </button>
+    <div className='flex justify-center items-center text-white font-semibold'>
+        <div className='flex justify-between items-center w-80 md:w-4/12 2xl:w-1/4 max-w-2xl md:mr-10'>
+          <span className='text-2xl ml-6 sm:ml-0 2xl:ml-24'>
+          {
+          router.pathname === '/addtoken' ?
+          'Add Token' :
+          'Edit Token'
+          }
+          </span>
+          <button
+          onClick={goToPreviousPage}
+          className='p-2 sm:px-8 2xl:mr-6 rounded-md
+          bg-klever-back-button'>
+            Voltar
+          </button>
+        </div>
+    </div> 
   );
 }
