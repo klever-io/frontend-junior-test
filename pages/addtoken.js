29da7a33daba0af77bd9f01bf3dc6b83bc1dd720
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-06 17:23:04 -0300

    style: Fix bugs in responsive css

--- pages/addtoken.js
+++ pages/addtoken.js
@@ -19,9 +19,9 @@ export default function Home() {
   return (
     <div>
       <BackButton/>
-      <div className='flex justify-center items-center'>
-        <div className='flex justify-between items-center w-80 md:w-4/12 2xl:w-1/4 max-w-2xl md:mr-10'>
-          <div className='flex justify-start items-center text-lg ml-6 sm:ml-0 2xl:ml-24'>
+      <div className='flex justify-center items-center '>
+        <div className='flex justify-center items-center md:w-4/12 2xl:w-1/4 max-w-2xl md:mr-10'>
+          <div className='flex justify-center items-center text-lg sm:ml-0 2xl:ml-24 '>
             {/* Inputs */}
             <form onSubmit={handleSubmit}>
               <div className='flex flex-col text-white font-bold mt-6'>
@@ -33,7 +33,7 @@ export default function Home() {
                     type='text'
                     value={token}
                     onChange={(e) => setToken(e.target.value)}
-                    className='bg-white rounded py-1 px-0 sm:px-3 md:px-20 2xl:px-28 border-2 focus:outline-none focus:border-transparent'
+                    className='bg-white rounded py-1 px-2 sm:px-3 md:px-20 2xl:px-28 border-2 focus:outline-none focus:border-transparent'
                   />
                 </div>
                 <div className='flex flex-col mt-3'>
@@ -44,7 +44,7 @@ export default function Home() {
                     type='text'
                     value={balance}
                     onChange={(e) => setBalance(e.target.value)}
-                    className='bg-white rounded py-1 px-0 sm:px-3 md:px-20 2xl:px-28 border-2 focus:outline-none focus:border-transparent'
+                    className='bg-white rounded py-1 px-2 sm:px-3 md:px-20 2xl:px-28 border-2 focus:outline-none focus:border-transparent'
                   />
                 </div>
               </div>
