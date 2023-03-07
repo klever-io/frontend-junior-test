9aa04640b5b40ef1b617380f8beeec7b006ad560
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-06 21:39:30 -0300

    feat: Now the tokens in Home Page is restored from localStorage

--- components/sections/TokensList.js
+++ components/sections/TokensList.js
@@ -1,30 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getLocalStorage } from '../../utils/localStorage';
+import numeral from 'numeral';
 
-const mockArray = [
-  {
-    name: 'KLV',
-    balance: '10,250.50',
-  },
-  {
-    name: 'DVK',
-    balance: '50,250.71',
-  },
-  {
-    name: 'KFI',
-    balance: '10,250.50',
-  },
-]
+import EditButton from '../buttons/EditButton';
 
 export default function TokensList() {
-  const [tokens, _setTokens] = useState(mockArray)
+  const [tokens, setTokens] = useState([]);
+
+  useEffect(() => {
+    restoreTokensOnload()
+  }, []);
+
+  const restoreTokensOnload = () => {
+    const tokensFromLocalStorage = getLocalStorage('tokensData');
+    setTokens(tokensFromLocalStorage);
+  };
 
   // Map of values of Tokens and Balance
   const listTokens = (tokens) => {
-    return tokens.map((token) => (
+    return tokens.map((token, index) => (
       <div className='flex justify-center items-center p-2'>
-        <div className='flex justify-between items-center p-2 w-80 sm:w-3/4 max-w-2xl font-black text-3xl'>
-          <span className='ml-4'>{token.name}</span>
-          <span>{token.balance}</span>
+        <EditButton className/>
+        <div key={index} className='flex justify-between items-center p-2 w-80 sm:w-3/4 max-w-2xl font-black text-3xl'>
+          <span className='ml-1'>{token.token}</span>
+          <span>{numeral(token.balance).format('0,0.00')}</span>
         </div>
       </div> 
     ));
@@ -32,7 +31,7 @@ export default function TokensList() {
 
 
   return (
-    tokens.length > 0 ? (
+    (tokens && tokens.length > 0) ? (
       <div className='text-white'>
         <div className='flex justify-center items-center p-2'>
           <div className='flex justify-between items-center p-2 w-80 sm:w-3/4 max-w-2xl font-semibold text-lg'>
@@ -43,7 +42,9 @@ export default function TokensList() {
         {listTokens(tokens)}
       </div>
     )
-    : 'Não há'
-  )
+    : <div className='flex justify-center mt-16'>
+        <h2 className='text-white font-black text-xl'>Não há tokens cadastrados</h2>
+      </div>
+  );
 }
 
