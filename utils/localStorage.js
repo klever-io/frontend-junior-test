cbfd7165926d7a66f1cd8039004183d9a5552f5a
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-14 10:56:33 -0300

    refactor: Refactor all localStorage functions

--- utils/localStorage.js
+++ utils/localStorage.js
@@ -1,28 +1,29 @@
-const getLocalStorage = (key) =>{
-  const allTokens = localStorage.getItem(key);
+const getLocalStorage = () =>{
+  const allTokens = localStorage.getItem('tokensData');
   return JSON.parse(allTokens);
 };
 
-const setLocalStorage = (token, balance) =>{
-  const tokensSavedInLocalStorage = getLocalStorage('tokensData');
-  let tokensData = [];
-
-  if (tokensSavedInLocalStorage) {
-    tokensData = tokensSavedInLocalStorage;
-  }
-
-  tokensData.push({token, balance});
-  localStorage.setItem('tokensData', JSON.stringify(tokensData));
+const setLocalStorage = (newTokenData) => {
+  const tokensFromLocalStorage = getLocalStorage() || [];
+  const newData = [ ...tokensFromLocalStorage, newTokenData ];
+  localStorage.setItem('tokensData', JSON.stringify(newData));
 };
 
-const updateLocalStorage = (tokenDataToUpdate, index) => {
-  const tokens = getLocalStorage('tokensData');
-  tokens[index] = tokenDataToUpdate;
-  setLocalStorage(tokens);
+const updateLocalStorage = (tokenDataToUpdate, idOfToken) => {
+  const tokensFromLocalStorage = getLocalStorage();
+  tokensFromLocalStorage.splice(idOfToken, 1, tokenDataToUpdate);
+  localStorage.setItem('tokensData', JSON.stringify(tokensFromLocalStorage));
+};
+
+const deleteLocalStorage = (idOfToken) => {
+  const tokensFromLocalStorage = getLocalStorage();
+  tokensFromLocalStorage.splice(idOfToken, 1);
+  localStorage.setItem('tokensData', JSON.stringify(tokensFromLocalStorage));
 };
 
 export {
   getLocalStorage,
   setLocalStorage,
   updateLocalStorage,
+  deleteLocalStorage,
 };
